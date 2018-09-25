c1b7e14486985923114e8b20d88a3adb04eec1ff
Pack Fan 1031 <ocskier@gmail.com>
2018-09-25 16:02:03 -0400

    latest revision

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
         // newProjDiv.append(newCardDiv);
         // $("#proj-row").append(newProjDiv);
 
-        var newCardA = $('<a class="carousel-item" href="'+projectsArray[i].Url+'" target="blank"></a>');
+        var newCardA = $('<a class="carousel-item" href="'+projectsArray[i].Url+'" target="blank" style="width:310px;"></a>');
         newCardA.append($('<img src="'+projectsArray[i].Pic+'" alt="Portfolio Pic" style="border-radius: 25px;">')).append($('<p class="ctr-txt white-text">'+projectsArray[i].Name+'</p>'));
         $("#proj-row").append(newCardA);
     }
