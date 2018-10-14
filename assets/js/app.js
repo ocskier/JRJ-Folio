6e3be884659656a4f98035f4820f20c0fcacb926
Pack Fan 1031 <ocskier@gmail.com>
2018-10-14 03:26:24 -0400

    adding contact as modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,22 +35,4 @@ $(document).ready(function() {
 
     //Get the HTML from the template   in the script tag
 
-    var from, name, textbody;
-    $("button").click(function () {
-        name = $("#contact-name").val();
-        from = $("#contact-email").val();
-        text = $("#contact-text").val();
-        $("#message").text("Sending E-mail...Please wait");
-        
-        $.get( "/send", {
-            name: name,    
-            from: from,
-            textbody: text},function(data){
-                if(data=="sent")
-                {
-                    $("#message").empty().html("Email has been sent to Jon!");
-                }
-            });
-    });
-
 });
